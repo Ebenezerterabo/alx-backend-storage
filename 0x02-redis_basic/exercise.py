e34042b85c7bd1d7385d67c0cc5e4a3977341208
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-29 04:55:00 +0000

    exercise file updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ Exercise module. """
 import redis
 import uuid
+from typing import Callable
 
 
 class Cache:
@@ -28,3 +29,45 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Callable = None) -> str:
+        """
+        Gets data from Redis.
+
+        Args:
+            key: The key at which the data is stored.
+            fn: The function to be applied to the data.
+
+        Returns:
+            The data stored at the key.
+        """
+        data = self._redis.get(key)
+
+        if fn:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Gets data from Redis and converts it to a string.
+
+        Args:
+            key: The key at which the data is stored.
+
+        Returns:
+            The data stored at the key as a string.
+        """
+        return str(self.get(key))
+
+    def get_int(self, key: str) -> int:
+        """
+        Gets data from Redis and converts it to an integer.
+
+        Args:
+            key: The key at which the data is stored.
+
+        Returns:
+            The data stored at the key as an integer.
+        """
+        return int(self.get(key))
